dbde8dc475c3bae6898ab8a399d33f0b29ab18eb
Vaughan_K <coachvaughan77@gmail.com>
2023-08-12 23:17:37 +1200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var cat = document.querySelector(".cat");
+var button = document.querySelector(".show-cat");
+button.addEventListener("click", function() {
+    cat.classList.add("show");
+});
+
+
+
