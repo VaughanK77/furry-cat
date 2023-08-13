222dee1acc2f89300e37fa40f0436a4ecfdb0e25
Vaughan_K <coachvaughan77@gmail.com>
2023-08-13 23:50:19 +1200

    Button styles added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 var cat = document.querySelector(".cat");
 var button = document.querySelector(".show-cat");
 button.addEventListener("click", function() {
-    cat.classList.add("show");
+    if (cat.classList.contains("show")) {
+        cat.classList.remove("show");
+        button.innerText = "Wait, come back!";
+        button.classList.add("disappear");
+        // console.log("Yes");
+    } else {
+        cat.classList.add("show");
+        button.innerText = "Shoo, cat!";
+        button.classList.remove("disappear");
+    // console.log("No");
+}
 });
 
 
